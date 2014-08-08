f16d47c33c0e1d3c8f16876fff34ede6358fb99f
nmanda01 <nikhil.manda@utdallas.edu>
2014-08-07 23:22:49 -0400

    Update DatabaseSeeder.php

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,74 +11,7 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		$this->call('UserTableSeeder');
 		
-		$this->call('ContactsTableSeeder');
 	}
 
-}
-
-
-class UserTableSeeder extends Seeder {
-
-    public function run()
-    {
-        DB::table('users')->delete();
-        
-        $users = array (
-        		 	array (
-        		 		'name' => 'Thom' ,
-        		 		'password' => Hash::make('Thom'),
-        		 		'email' => 'tyorke@radiohead.com'
-        		 		),
-        		 		
-        		 		
-        		 		
-        		 	);
-
-		DB::table('users')->insert($users);
-    }
-}
-
-class ContactsTableSeeder extends Seeder {
-
-    public function run()
-    {
-        DB::table('contacts')->delete();
-        
-        $contacts = array (
-        		 	
-        		 		array (
-        		 			'user_id' => 3,
-        		 			'name' => 'Jonny' ,
-        		 			'phoneNumber' => 2432144444,
-        		 			'emailAddress' => 'jgreenwood@radiohead.com'
-        		 			),
-        		 			
-        		 		array (
-        		 			'user_id' => 3,
-        		 			'name' => 'Colin',
-        		 			'phoneNumber' => 9244586167,
-        		 			'emailAddress' => 'cgreenwood@radiohead.com'
-        		 			),
-        		 			
-        		 		array (
-        		 			'user_id' => 3,
-        		 			'name' => 'Ed',
-        		 			'phoneNumber' => 1756182912,
-        		 			'emailAddress' => 'eobrian@radiohead.com'
-        		 			),
-        		 			
-        		 		array (
-        		 			'user_id' => 3,
-        		 			'name' => 'Phil',
-        		 			'phoneNumber' => 7730343987,
-        		 			'emailAddress' => 'pselway@radiohead.com'
-        		 			)
-        		 		
-        		 		
-        		 	) 	;
-
-		DB::table('contacts')->insert($contacts);
-    }
 }
