ccf68a29a3854634ae8afb616238e4dd7d4cf102
nmanda01 <nikhil.manda@utdallas.edu>
2014-08-07 23:26:29 -0400

    Update User.php

--- app/models/User.php
+++ app/models/User.php
@@ -24,10 +24,12 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	protected $hidden = array('password', 'remember_token');
 	
 	
+	//This is the only part I coded or the User model:
+	
 	public function contacts() {
 	
 	
-	//return $this->hasMany('Contact');
+	//Calling this method for the user who is logged in will display all the contacts that belong to said user. 
 	
 
 	return $this->hasMany('Contact' , 'user_id');
